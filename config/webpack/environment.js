669e7f1a2842eeb31b4bb014c378ff9e88cd376e
imhta <imhtapm@gmail.com>
2019-09-26 22:03:57 +0530

    bootstrap and profile.dev added

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const webpack = require('webpack')
+environment.plugins.append(
+  'Provide',
+  new webpack.ProvidePlugin({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default']
+  })
+)
