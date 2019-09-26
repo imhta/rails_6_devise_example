2b76b480e9ec84ac2d3520b1a3c2a9bb8fe59eb7
imhta <imhtapm@gmail.com>
2019-09-26 22:09:36 +0530

    Devise mailer config for development env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,6 +28,8 @@ Rails.application.configure do
     config.cache_store = :null_store
   end
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
