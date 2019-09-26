8f74daf4c5b5ff6989334c2bf9a5a8d94a8202a7
imhta <imhtapm@gmail.com>
2019-09-27 00:13:57 +0530

    removed uneccesary action in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,7 @@ class ApplicationController < ActionController::Base
   
     def home
     end
-  
-    def about
-    end
-  
-    def contact
-    end
-  
-    def secret
-    end
+
   
     protected
   
