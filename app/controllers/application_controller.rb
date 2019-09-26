9d94fb30f012b06b9a9ed63199d58c09593c717e
imhta <imhtapm@gmail.com>
2019-09-27 00:13:07 +0530

    added user device views and added application views and actions in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    before_action :configure_permitted_parameters, if: :devise_controller?
+    before_action :authenticate_user!, except: [:home, :about, :contact]
+  
+    def home
+    end
+  
+    def about
+    end
+  
+    def contact
+    end
+  
+    def secret
+    end
+  
+    protected
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up)
+      devise_parameter_sanitizer.permit(:account_update)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'application#home'
+  
+  get '/home', to: 'application#home', as: 'home'
+
 end
