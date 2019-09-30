9fd89f32e1bc682969e12c94c969da34be731459
imhta <imhtapm@gmail.com>
2019-09-30 21:20:10 +0530

    add authorization for home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
     protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
-    before_action :authenticate_user!, except: [:home, :about, :contact]
+    before_action :authenticate_user!
   
     def home
     end
