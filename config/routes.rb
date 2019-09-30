7b2f19ebb954406c5892531701732b8ea17940ad
imhta <imhtapm@gmail.com>
2019-09-30 16:30:21 +0530

    added devise scope to route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root to: 'application#home'
-  
-  get '/home', to: 'application#home', as: 'home'
+
+
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    get 'sign_up', to: 'devise/registrations#new'
+  end
+
 
 end
