91e24eb4d1ec8ad0dc7109e4a4821d694b245283
imhta <imhtapm@gmail.com>
2019-09-30 16:50:58 +0530

    controllers generated and route config

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
-  devise_for :users
-
-  root to: 'application#home'
 
 
+
+  devise_for :users, controllers: {
+    sessions: 'user/sessions',
+    registrations: 'user/registrations'
+  }
   devise_scope :user do
-    get 'sign_in', to: 'devise/sessions#new'
-    get 'sign_up', to: 'devise/registrations#new'
+    get 'sign_in', to: 'user/sessions#new'
+    get 'sign_up', to: 'user/registrations#new'
+    root to: 'user/sessions#new'
   end
 
+  get 'home', to: 'application#home'
 
 end
