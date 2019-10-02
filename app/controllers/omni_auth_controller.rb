9b6e98e59867e878f66ddc9e1d6961e107cc8f29
imhta <imhtapm@gmail.com>
2019-10-02 17:44:30 +0530

    added omniauth succesfully

---
+++ app/controllers/omni_auth_controller.rb
@@ -0,0 +1,6 @@
+class OmniAuthController < Devise::OmniauthCallbacksController
+    def github
+        @user = User.from_omniauth(request.env["omniauth.auth"])
+        sign_in_and_redirect @user
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions',
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'omni_auth'
   }
   devise_scope :user do
     get 'sign_in', to: 'users/sessions#new'
