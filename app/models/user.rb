161200a5280428e4260bab2acc80db684044953f
imhta <imhtapm@gmail.com>
2019-10-02 17:51:37 +0530

    trackable property in user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:github]
 
   def self.from_omniauth(auth)
